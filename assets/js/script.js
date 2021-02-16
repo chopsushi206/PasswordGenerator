88135104cb078a14cf25ca6da45ff1e5bed513bd
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-16 10:16:09 -0800

    added random character variable functions to script.js and edited some basic stying in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//Password Attribute Variables: LCase, UCase, Num, Special, & Length
+
+function randomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+};
+
+function randomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
+};
+
+function randomNumber () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
+};
+
+function specialChars () {
+  var special = "!@#$%^&*()-_:;./?~`'";
+  return special[Math.floor(Math.random() * special.length)]; 
+};
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
