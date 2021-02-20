9b8f1bec59d50850aa495bcc153ccc71c4ded482
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-20 14:30:32 -0800

    Removed prompts and most if else statements. Rewrote code to include event listeners to checkboxes and input fields instead. Added a function to input field so that user cannot exceed maximum chactacter length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,132 +1,78 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passLength;
+//DOM Elements
 
-//Password Attribute Variables: LCase, UCase, Num, Special Chars
+const resultElement = document.getElementById('password');
+const lengthElement = document.getElementById('length');
+const upperElement = document.getElementById('upper');
+const lowerElement = document.getElementById('lower');
+const numberElement = document.getElementById('number');
+const symbolElement = document.getElementById('symbol');
+const generateBtn = document.getElementById('generate');
 
-function randomLower () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+// Functions to generate random Lowercase Uppercase Number and Symbol
+
+function getLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 
-function randomUpper () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
+function getUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
-function randomNumber () {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
+function getNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 };
 
-function specialChars () {
-  var special = "!@#$%^&*()-_:;./?~`'";
-  return special[Math.floor(Math.random() * special.length)]; 
+function getSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-// Function to retrieve Password length
 
-function chooseLength () {
-  passLength = prompt ("How long would you like your password? Choose between 8-128")
+// Object for functions for Uppecase, Lowercase, Number, and Symbol
+const randomFunc = {
+  lower: getLower,
+  upper: getUpper,
+  number: getNumber,
+  symbol: getSymbol
+};
 
-  if (passLength<8) {
-    alert("Please choose a number between 8-128");
-    chooseLength();
-  } else if (passLength>128) {
-    alert("Please choose a number between 8-128");
-    chooseLength();
-  } else if (isNaN(passLength)) {
-    alert("Please choose a number between 8-128");
-    chooseLength();
-  } else {
-    return passLength;
+//Function to limit Input Values for password length
+function inputChange(input) {
+  if (input.value < 8) input.value = 8;
+  if (input.value > 128) input.value = 128;
+};
+
+// Event Listener to grab following elements when button is clicked, also sets result of generate password function to textbox
+generateBtn.addEventListener('click', () => {
+  const length = +lengthElement.value;
+  const hasLower = lowerElement.checked;
+  const hasUpper = upperElement.checked;
+  const hasNumber = numberElement.checked;
+  const hasSymbol = symbolElement.checked;
+
+  resultElement.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+  // Doesn't have a selected type
+  if (typesCount === 0) {
+    return '';
   }
-};
 
-// Function to include or exclude Lower Case Letters in Password
-
-function chooseLowerCase() {
-  lowercasePrompt = prompt("Do you want to include LowerCase letters in your password? \n(Yes or No)");
-  lowercasePrompt = lowercasePrompt.toLowerCase();
-
-  if (lowercasePrompt === null || lowercasePrompt === "") {
-    alert("Please answer Yes or No");
-    chooseLowerCase();
-
-  } else if (lowercasePrompt === "yes" || lowercasePrompt === "y") {
-    lowercasePrompt = true;
-    return lowercasePrompt;
-
-  } else if (lowercasePrompt === "no" || lowercasePrompt === "n") {
-    lowercasePrompt = false;
-    return lowercasePrompt;
-
-  } else {
-    alert("Please answer Yes or No");
-    chooseLowerCase();
+  // creates a loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
   }
-  return lowercasePrompt;
-};
 
-var lower = chooseLowerCase();
+  const finalPassword = generatedPassword.slice(0, length);
 
-// Function to include or exclude Upper Case Letters in Password
-
-function chooseUpperCase() {
-  uppercasePrompt = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
-  uppercasePrompt = uppercasePrompt.toLowerCase();
-
-  if (uppercasePrompt === null || uppercasePrompt === "") {
-    alert("Please answer Yes or No");
-    chooseUpperCase();
-
-  } else if (uppercasePrompt === "yes" || uppercasePrompt === "y") {
-    uppercasePrompt = true;
-    return uppercasePrompt;
-
-  } else if (uppercasePrompt === "no" || uppercasePrompt === "n") {
-    uppercasePrompt = false;
-    return uppercasePrompt;
-
-  } else {
-    alert("Please answer Yes or No");
-    chooseUpperCase();
-  }
-  return uppercasePrompt;
-};
-
-var upper = chooseUpperCase();
-
-// Function to include Numbers in the password
-
-function chooseNumber() {
-  numberPrompt = prompt("Do you want to include numbers in your password? \n(Yes or No)");
-  numberPrompt = numberPrompt.toLowerCase();
-
-  if (numberPrompt === null || numberPrompt === "") {
-    alert("Please answer Yes or No");
-    chooseNumber();
-
-  } else if (numberPrompt === "yes" || numberPrompt === "y") {
-    numberPrompt = true;
-    return numberPrompt;
-
-  } else if (numberPrompt === "no" || numberPrompt === "n") {
-    numberPrompt = false;
-    return numberPrompt;
-
-  } else {
-    alert("Please answer Yes or No");
-    chooseNumber();
-  }
-  return numberPrompt;
-};
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  return finalPassword;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
